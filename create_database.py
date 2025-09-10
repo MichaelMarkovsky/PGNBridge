d1fd16e784faf62902ee26c44291545eda85a166
Michael <misha.mar2001@gmail.com>
2025-09-11 01:21:03 +0300

    MODIFIED: get all games

--- create_database.py
+++ create_database.py
@@ -14,6 +14,8 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0'
 }
 
+#=================================Get Archives=======================
+
 url = f"https://api.chess.com/pub/player/{username}/games/archives"
 
 archives = requests.get(url,headers=headers)
@@ -21,5 +23,31 @@ archives = requests.get(url,headers=headers)
 #print(archives.text)
 #print(archives.status_code)
 
+archives_list = []
+
+print(f"{username}'s archives:")
 for date in json.loads(archives.text)["archives"]:
+    archives_list.append(date)
     print(date)
+
+
+
+#==================================Get Games=======================
+
+games = [] # a list of tuples of (game url , pgn)
+
+for month in archives_list:
+    resp = requests.get(month, headers=headers) # the games of the month that have been played
+    for game in json.loads(resp.text)["games"]:
+        url = game.get("url")
+        pgn = game.get("pgn")
+        if url and pgn:  # only add if both exist (pgn might not exist if the game was aborted)
+            games.append((url, pgn)) # add each game to the list
+
+
+print(games)
+print(len(games))
+
+
+#==================================Create Data-base=======================
+# create the database if it does not exist
